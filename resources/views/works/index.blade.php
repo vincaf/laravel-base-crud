02527111b3f4deef368ce573c5dfa8f6809144ad
vincaf <vincenzocafassog@gmail.com>
2022-09-14 01:29:11 +0200

    feat: add footer

--- resources/views/works/index.blade.php
+++ resources/views/works/index.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Main Works')
 
 @section('main-content')
-    <div class="container">
+    <div class="container my-3">
         <div class="row">
             <div class="col-12">
                 <h3>MAIN CONTENT</h3>
