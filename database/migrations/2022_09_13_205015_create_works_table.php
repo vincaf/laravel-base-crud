407eed690783cdc5d8f0c5313026d5c9b8a2bf98
vincaf <vincenzocafassog@gmail.com>
2022-09-13 23:05:03 +0200

    feat: migrate table

--- database/migrations/2022_09_13_205015_create_works_table.php
+++ database/migrations/2022_09_13_205015_create_works_table.php
@@ -15,6 +15,13 @@ class CreateWorksTable extends Migration
     {
         Schema::create('works', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('thumb')->unique();
+            $table->float('price', 5, 2);
+            $table->string('series');
+            $table->date('sale_date');
+            $table->string('type');
             $table->timestamps();
         });
     }
