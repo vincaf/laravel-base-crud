0a8f150870677cb07aac3fbc170b3f6d42010cac
vincaf <vincenzocafassog@gmail.com>
2022-09-13 22:47:43 +0200

    feat: extend layout and add main example

---
+++ resources/views/works/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('title', 'Main Works')
+
+@section('main-content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h3>MAIN CONTENT</h3>
+            </div>
+        </div>
+    </div>
+@endsection
