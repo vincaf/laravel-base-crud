565c85986c3652a8a139a38f06979f7d5ff23127
vincaf <vincenzocafassog@gmail.com>
2022-09-14 00:38:38 +0200

    feat: add work values with seeder

--- database/seeds/WorksTableSeeder.php
+++ database/seeds/WorksTableSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Work;
+use Faker\Generator as Faker;
 use Illuminate\Database\Seeder;
 
 class WorksTableSeeder extends Seeder
@@ -9,8 +11,24 @@ class WorksTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        $workTypes = [
+            'comic book',
+            'graphic novel',
+        ];
+
+        for ($i=0; $i < 100; $i++) { 
+            $newWork = new Work();
+            $newWork->title = $faker->realText(35);
+            $newWork->description = $faker->paragraph(3, true);
+            $newWork->thumb = $faker->imageUrl(500, 400, 'comics');
+            $newWork->price = $faker->randomFloat(2, 1, 50);
+            $newWork->series = $faker->name();
+            $newWork->sale_date = $faker->date();
+            $newWork->type = $faker->randomElement($workTypes);
+
+            $newWork->save();
+        }
     }
 }
