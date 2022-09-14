ef1c56cf520820215db9eaad3df7dcdbe868cb69
vincaf <vincenzocafassog@gmail.com>
2022-09-14 17:53:43 +0200

    feat: create show blade for works

---
+++ resources/views/works/show.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+
+@section('title', 'Comics')
+
+@section('main-content')
+    <div class="container my-5 py-2">
+        <div class="row">
+            
+        </div>
+    </div>
+@endsection
