fbb1255786c8ec3b2660cb73595c47725ee38ac8
vincaf <vincenzocafassog@gmail.com>
2022-09-19 01:52:24 +0200

    refactor(404): change img and structure

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -3,11 +3,7 @@
 @section('title', '404 NOT FOUND')
 
 @section('main-content')
-    <div class="container my-5 py-2">
-        <div class="row justify-content-center">
-            <div class="col-10">
-                <img id="jumbo" src="https://static.vecteezy.com/system/resources/previews/000/096/160/original/vector-comic-style-404-error-illustration.jpg" alt="404">
-            </div>
-        </div>
+    <div class="container-fluid">
+        <img id="jumbo" src="https://tinypic.host/images/2022/09/19/40473e590dd769c9e49.png" alt="404">
     </div>
 @endsection
