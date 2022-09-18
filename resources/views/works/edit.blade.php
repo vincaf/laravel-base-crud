4c6b5b4a3a06ec444814db7a412e5363fa8c3dd6
vincaf <vincenzocafassog@gmail.com>
2022-09-19 01:21:09 +0200

    feat(edit): add on error returns the old value

--- resources/views/works/edit.blade.php
+++ resources/views/works/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.main')
 
-@section('title', 'Create new comic')
+@section('title', 'Edit comic')
 
 @section('main-content')
     <div class="container my-5 py-2">
@@ -24,7 +24,7 @@
 
                     <div class="mb-3">
                         <label for="input-title" class="form-label">Title</label>
-                        <input type="text" class="form-control" id="input-title" name="title" value="{{ $work->title }}" required>
+                        <input type="text" class="form-control" id="input-title" name="title" value="{{ old('title', $work->title) }}" required>
                         @error('title')
                             <div class="alert alert-danger">
                                 {{ $message }}
@@ -33,7 +33,7 @@
                     </div>
                     <div class="mb-3">
                         <label for="input-description" class="form-label">Description</label>
-                        <textarea class="form-control" id="input-description" cols="30" rows='8' name="description" required>{{ $work->description }}</textarea>
+                        <textarea class="form-control" id="input-description" cols="30" rows='8' name="description" required>{{ old('description', $work->description) }}</textarea>
                         @error('description')
                             <div class="alert alert-danger">
                                 {{ $message }}
@@ -42,7 +42,7 @@
                     </div>
                     <div class="mb-3">
                         <label for="input-thumb" class="form-label">Thumb</label>
-                        <input type="text" class="form-control" id="input-thumb" name="thumb" value="{{ $work->thumb }}" required>
+                        <input type="text" class="form-control" id="input-thumb" name="thumb" value="{{ old('thumb', $work->thumb) }}" required>
                         @error('thumb')
                             <div class="alert alert-danger">
                                 {{ $message }}
@@ -51,7 +51,7 @@
                     </div>
                     <div class="mb-3">
                         <label for="input-price" class="form-label">Price</label>
-                        <input type="number" step="0.01" class="form-control" id="input-price" name="price" value="{{ $work->price }}" required>
+                        <input type="number" step="0.01" class="form-control" id="input-price" name="price" value="{{ old('price', $work->price) }}" required>
                         @error('price')
                             <div class="alert alert-danger">
                                 {{ $message }}
@@ -60,7 +60,7 @@
                     </div>
                     <div class="mb-3">
                         <label for="input-series" class="form-label">Series</label>
-                        <input type="text" class="form-control" id="input-series" name="series" value="{{ $work->series }}" required>
+                        <input type="text" class="form-control" id="input-series" name="series" value="{{ old('series', $work->series) }}" required>
                         @error('series')
                             <div class="alert alert-danger">
                                 {{ $message }}
@@ -69,7 +69,7 @@
                     </div>
                     <div class="mb-3">
                         <label for="input-sale_date" class="form-label">Sale date</label>
-                        <input type="date" class="form-control" id="input-sale_date" name="sale_date" value="{{ $work->sale_date }}" required>
+                        <input type="date" class="form-control" id="input-sale_date" name="sale_date" value="{{ old('sale_date', $work->sale_date) }}" required>
                         @error('sale_date')
                             <div class="alert alert-danger">
                                 {{ $message }}
