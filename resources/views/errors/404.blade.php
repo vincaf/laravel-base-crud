2f8820a0163ce4200f2f65c79c1b321d177c467d
vincaf <vincenzocafassog@gmail.com>
2022-09-19 01:40:48 +0200

    feat(404): add image for error 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('title', '404 NOT FOUND')
+
+@section('main-content')
+    <div class="container my-5 py-2">
+        <div class="row justify-content-center">
+            <div class="col-10">
+                <img id="jumbo" src="https://static.vecteezy.com/system/resources/previews/000/096/160/original/vector-comic-style-404-error-illustration.jpg" alt="404">
+            </div>
+        </div>
+    </div>
+@endsection
